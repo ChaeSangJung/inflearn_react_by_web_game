a4c7796806d3d685a40266938fac16e4f271e917
ChaeSangJung <shtingstar@hanmail.net>
2020-08-24 23:23:36 +0900

    0824

--- src/Gugudan.js
+++ src/Gugudan.js
@@ -17,11 +17,13 @@ class Gugudan extends React.Component {
     onSubmit = (e) => {
         e.preventDefault();
         if(parseInt(this.state.value) === this.state.first * this.state.second){
-            this.setState({
-                result: `${this.state.first} 곱하기 ${this.state.second} = ${this.state.value} 정답!`,
-                first: Math.ceil(Math.random() * 9),
-                second: Math.ceil(Math.random() * 9),
-                value: ''                
+            this.setState((prevState) => {
+                return {
+                    result: `${this.state.first} 곱하기 ${this.state.second} = ${prevState.value} 정답!`,
+                    first: Math.ceil(Math.random() * 9),
+                    second: Math.ceil(Math.random() * 9),
+                    value: ''                
+                };                
             });
         } else {
             this.setState({
